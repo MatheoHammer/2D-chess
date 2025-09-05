acefcc796499931850a38bc3e2a5480a3b41a5d2
Sen <seina002@osloskolen.no>
2025-09-05 14:28:06 +0200

    Update Pieces.py

--- objects/Pieces.py
+++ objects/Pieces.py
@@ -8,3 +8,28 @@ class Pawn(Piece):
     def __init__(self, x, y, color):
         super().__init__(x, y, color)
         self.type = "pawn"
+
+class Rook(Piece):
+    def __init__(self, x, y, color):
+        super().__init__(x, y, color)
+        self.type = "pawn"
+
+class Queen(Piece):
+    def __init__(self, x, y, color):
+        super().__init__(x, y, color)
+        self.type = "pawn"
+
+class King(Piece):
+    def __init__(self, x, y, color):
+        super().__init__(x, y, color)
+        self.type = "pawn"
+
+class Knight(Piece):
+    def __init__(self, x, y, color):
+        super().__init__(x, y, color)
+        self.type = "pawn"
+
+class Bishop(Piece):
+    def __init__(self, x, y, color):
+        super().__init__(x, y, color)
+        self.type = "pawn"
