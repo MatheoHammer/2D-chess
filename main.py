46899bdd8dcf45112780a747e9c204488e652cbd
Sen <seina002@osloskolen.no>
2025-09-05 14:38:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-
-
 import itertools
 import pygame as pg
-
 
 pg.init()
 
@@ -12,33 +9,37 @@ WHITE = pg.Color('white')
 screen = pg.display.set_mode((500, 500))
 clock = pg.time.Clock()
 
-colors = itertools.cycle((WHITE, BLACK))
-tile_size = 40
-width, height = 8*tile_size, 8*tile_size
-background = pg.Surface((width, height))
+def create_background():
+    colors = itertools.cycle((WHITE, BLACK))
+    tile_size = 40
+    width, height = 8*tile_size, 8*tile_size
+    background = pg.Surface((width, height))
 
-for y in range(0, height, tile_size):
-    for x in range(0, width, tile_size):
-        rect = (x, y, tile_size, tile_size)
-        pg.draw.rect(background, next(colors), rect)
-    next(colors)
+    for y in range(0, height, tile_size):
+        for x in range(0, width, tile_size):
+            rect = (x, y, tile_size, tile_size)
+            pg.draw.rect(background, next(colors), rect)
+        next(colors)
 
-game_exit = False
-while not game_exit:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            game_exit = True
-
-    screen.fill((60, 70, 90))
-    screen.blit(background, (100, 100))
-
-    pg.display.flip()
-    clock.tick(30)
-
-pg.quit()
+    return background
 
 def main():
-    pass
+    running = True
+
+    background = create_background()
+
+    while running:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+
+        screen.fill((60, 70, 90))
+        screen.blit(background, (100, 100))
+
+        pg.display.flip()
+        clock.tick(30)
+
+    pg.quit()
 
 if __name__ == "__main__":
     main()
